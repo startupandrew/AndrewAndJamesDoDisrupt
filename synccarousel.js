5ed5757da339d0aa603794c3755ca1d7c939909a
startupandrew <startupandrew@gmail.com>
2012-09-09 04:44:09 -0700

    drawing

--- synccarousel.js
+++ synccarousel.js
@@ -4,6 +4,8 @@
  * @constructor
  */
 
+var allSlides = [];
+
 displayCarousel = function(userdata, carousel, slideholder) {
 
   //set up the carousel
@@ -14,7 +16,9 @@ displayCarousel = function(userdata, carousel, slideholder) {
   var slides = userdata.child("slides");
   slides.on("child_added", function(urlSnap) {
     var imgURL = urlSnap.val();
-    slideholder.append("<div class='item'><img src='" + imgURL + "' style='width: 100%; height: 100%;'></div>");
+    var newSlide = new DrawableSlide(userdata, imgURL, allSlides.length);
+    slideholder.append(newSlide.slide);
+    allSlides.push(newSlide);
     carousel.carousel("next");
   });
 
@@ -26,3 +30,27 @@ displayCarousel = function(userdata, carousel, slideholder) {
     }
   });
 }
+
+DrawableSlide = function(userdata, url, slideNum) {
+  var newSlide = $("<div class='item' style='width: 100%; height: 600px; position: relative;'></div>");
+  var con = $("<div></div>");
+  newSlide.append(con);
+  con.append("<img src='" + url + "' style='position: absolute; left: 0; top: 0; width: 100%; height: 100%;'>");
+  var canv = $("<canvas style='position: absolute; left: 0; top: 0; width: 100%; height: 100%;' width=720 height=540></canvas>");
+  con.append(canv);
+
+  var c=canv.get(0);
+  var ctx=c.getContext("2d");
+  ctx.strokeStyle="#FF0000";
+
+  userdata.child("drawings").child(slideNum).on("child_added", function(snap) {
+    var line = snap.val();
+
+    ctx.moveTo(line.old.x,line.old.y);
+    ctx.lineTo(line.new.x,line.new.y);
+    ctx.stroke();
+  });
+
+  this.slide = newSlide;
+}
+
